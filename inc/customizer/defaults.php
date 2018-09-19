5b836413419f44eeb59beab5770b47e2b6ef3dbb
David Rozando <1287713+daviedR@users.noreply.github.com>
2018-09-19 21:01:44 +0700

    Add default selected colors on header bars

--- inc/customizer/defaults.php
+++ inc/customizer/defaults.php
@@ -309,9 +309,9 @@ $add['header_top_bar_submenu_letter_spacing'] = '';
 
 $add['header_top_bar_icon_size'] = '18px';
 
-$add['header_top_bar_bg_color'] = '';
-$add['header_top_bar_border_color'] = '';
-$add['header_top_bar_text_color'] = '';
+$add['header_top_bar_bg_color'] = $colors['bg'];
+$add['header_top_bar_border_color'] = $colors['border'];
+$add['header_top_bar_text_color'] = $colors['text'];
 $add['header_top_bar_link_text_color'] = $colors['heading'];
 $add['header_top_bar_link_hover_text_color'] = $colors['text'];
 $add['header_top_bar_menu_highlight_color'] = $colors['accent'];
@@ -357,9 +357,9 @@ $add['header_main_bar_submenu_letter_spacing'] = '';
 
 $add['header_main_bar_icon_size'] = '18px';
 
-$add['header_main_bar_bg_color'] = '';
-$add['header_main_bar_border_color'] = '';
-$add['header_main_bar_text_color'] = '';
+$add['header_main_bar_bg_color'] = $colors['bg'];
+$add['header_main_bar_border_color'] = $colors['border'];
+$add['header_main_bar_text_color'] = $colors['text'];
 $add['header_main_bar_link_text_color'] = $colors['heading'];
 $add['header_main_bar_link_hover_text_color'] = $colors['text'];
 $add['header_main_bar_menu_highlight_color'] = $colors['accent'];
@@ -407,9 +407,9 @@ $add['header_bottom_bar_submenu_letter_spacing'] = '';
 
 $add['header_bottom_bar_icon_size'] = '18px';
 
-$add['header_bottom_bar_bg_color'] = '';
-$add['header_bottom_bar_border_color'] = '';
-$add['header_bottom_bar_text_color'] = '';
+$add['header_bottom_bar_bg_color'] = $colors['bg'];
+$add['header_bottom_bar_border_color'] = $colors['border'];
+$add['header_bottom_bar_text_color'] = $colors['text'];
 $add['header_bottom_bar_link_text_color'] = $colors['heading'];
 $add['header_bottom_bar_link_hover_text_color'] = $colors['text'];
 $add['header_bottom_bar_menu_highlight_color'] = $colors['accent'];
@@ -472,9 +472,9 @@ $add['header_mobile_vertical_bar_submenu_letter_spacing'] = '';
 
 $add['header_mobile_vertical_bar_icon_size'] = '18px';
 
-$add['header_mobile_vertical_bar_bg_color'] = '';
-$add['header_mobile_vertical_bar_border_color'] = '';
-$add['header_mobile_vertical_bar_text_color'] = '';
+$add['header_mobile_vertical_bar_bg_color'] = $colors['bg'];
+$add['header_mobile_vertical_bar_border_color'] = $colors['border'];
+$add['header_mobile_vertical_bar_text_color'] = $colors['text'];
 $add['header_mobile_vertical_bar_link_text_color'] = $colors['heading'];
 $add['header_mobile_vertical_bar_link_hover_text_color'] = $colors['text'];
 $add['header_mobile_vertical_bar_menu_highlight_color'] = $colors['accent'];
