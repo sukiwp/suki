2705434ce4dc3c0bde86a88fd446c08edc7dcbf1
David Rozando <1287713+daviedR@users.noreply.github.com>
2018-10-26 16:25:44 +0700

    Improve Suki Pro teaser strings

--- inc/customizer/options/_sections.php
+++ inc/customizer/options/_sections.php
@@ -369,6 +369,25 @@ $wp_customize->add_panel( $panel, array(
 		'priority'    => 30,
 	) );
 
+	if ( suki_show_pro_teaser() ) {
+		// ------
+		$wp_customize->add_section( new Suki_Customize_Section_Spacer( $wp_customize, 'suki_section_spacer_footer_advanced', array(
+			'panel'       => $panel,
+			'priority'    => 90,
+		) ) );
+
+		// More Options Available on Suki Pro
+		$wp_customize->add_section( new Suki_Customize_Section_Pro_Teaser( $wp_customize, 'suki_section_pro_footer_advanced', array(
+			'title'       => esc_html_x( 'More Options on Suki Pro', 'Suki Pro upsell', 'suki' ),
+			'panel'       => $panel,
+			'url'         => SUKI_PRO_URL,
+			'features'    => array(
+				esc_html_x( 'Dynamic widgets column width (responsive)', 'Suki Pro upsell', 'suki' ),
+			),
+			'priority'    => 90,
+		) ) );
+	}
+
 // ------
 $wp_customize->add_section( new Suki_Customize_Section_Spacer( $wp_customize, 'suki_section_spacer_180', array(
 	'priority'    => 180,
