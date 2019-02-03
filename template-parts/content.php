91d7b01ff926d197aeb12fd3b0450812d943c518
David Rozando <david.rozando@gmail.com>
2019-02-04 01:46:26 +0700

    Make blog posts in archive page render excerpt if specified

--- template-parts/content.php
+++ template-parts/content.php
@@ -47,21 +47,31 @@
 			 * Hook: suki/frontend/entry/before_content
 			 */
 			do_action( 'suki/frontend/entry/before_content' );
-			
-			// Print the content.
-			the_content(
-				sprintf(
-					/* translators: %s: current post title. */
-					esc_html__( 'Continue Reading %s', 'suki' ),
-					the_title( '<span class="screen-reader-text">', '</span>', false )
-				)
-			);
 
-			// Print content pagination, if exists.
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'suki' ),
-				'after'  => '</div>',
-			) );
+			// If it's included in a single post page.
+			if ( is_single() ) {
+				// Print the content.
+				the_content();
+
+				// Print content pagination, if exists.
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'suki' ),
+					'after'  => '</div>',
+				) );
+			}
+			// If it's included in a posts archive page.
+			else {
+				// Print excerpt if specified.
+				if ( has_excerpt() ) {
+					the_excerpt();
+				}
+				// Print full content.
+				else {
+					the_content(
+						apply_filters( 'suki/frontend/read_more_text', esc_html__( 'Continue Reading', 'suki' ) ) . the_title( ' <span class="screen-reader-text">', '</span>', false )
+					);
+				}
+			}
 			
 			/**
 			 * Hook: suki/frontend/entry/after_content
