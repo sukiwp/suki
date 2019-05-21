33f804342f76d7a0422ebdec97aaa45f5864270b
David Rozando <david.rozando@gmail.com>
2019-05-21 17:16:54 +0700

    Add Brizy Integration option: disable Brizy's reset CSS

--- inc/compatibilities/brizy/class-suki-compatibility-brizy.php
+++ inc/compatibilities/brizy/class-suki-compatibility-brizy.php
@@ -43,8 +43,16 @@ class Suki_Compatibility_Brizy {
 			// Add editor CSS to replace default (fixed) 1170px content width with theme content container (wrapper) width.
 			add_action( 'brizy_editor_enqueue_scripts', array( $this, 'use_theme_container_width_on_editor' ) );
 
-			// Modify generated frontend CSS to replace default (fixed) 1170px content width with theme content container (wrapper) width.
+			// Modify generated Brizy head content: Replace default (fixed) 1170px content width with theme content container (wrapper) width.
 			add_filter( 'brizy_content', array( $this, 'use_theme_container_width_on_frontend' ), 10, 3 );
+		}
+
+		if ( suki_get_theme_mod( 'brizy_disable_reset_css' ) ) {
+			// Add editor CSS to unset all Brizy reset CSS.
+			add_action( 'brizy_editor_enqueue_scripts', array( $this, 'disable_reset_css_on_editor' ) );
+			
+			// Add frontend CSS to unset all Brizy reset CSS.
+			add_action( 'brizy_preview_enqueue_scripts', array( $this, 'disable_reset_css_on_frontend' ) );
 		}
 
 		// Customizer settings & values
@@ -58,14 +66,14 @@ class Suki_Compatibility_Brizy {
 	 */
 
 	/**
-	 * Add additional CSS to Brizy editor.
+	 * Replace default (fixed) 1170px content width with theme content container (wrapper) width on editor.
 	 */
 	public function use_theme_container_width_on_editor() {
 		wp_add_inline_style( 'brizy-editor', suki_minify_css_string( '.brz-ed .brz-section__content[style*="--containerWidth:1170px"] { --containerWidth: ' . suki_get_theme_mod( 'container_width' ) . ' !important; }' ) );
 	}
 
 	/**
-	 * Add additional CSS to Brizy frontend.
+	 * Modify generated Brizy head content: Replace default (fixed) 1170px content width with theme content container (wrapper) width.
 	 *
 	 * @param string $content
 	 * @param Brizy_Editor_Project $project
@@ -73,11 +81,25 @@ class Suki_Compatibility_Brizy {
 	 * @return string
 	 */
 	public function use_theme_container_width_on_frontend( $content, $project, $post ) {
-		if ( ! did_action( 'wp_body_open' ) ) {
-			$content = preg_replace( '/max-width:1170px;/', 'max-width:' . suki_get_theme_mod( 'container_width' ) . ';', $content );
+		if ( '<meta ' === substr( $content, 0, 6 ) ) {
+			$content = preg_replace( '/(\.css-(.*?),\[data-css-\2\])\{max-width:1170px;/', '$1{max-width:' . suki_get_theme_mod( 'container_width' ) . ';', $content );
 		}
 
 		return $content;
+	}
+
+	/**
+	 * Unset all Brizy reset CSS on editor.
+	 */
+	public function disable_reset_css_on_editor() {
+		wp_add_inline_style( 'brizy-editor', suki_minify_css_string( '.brz .brz-root__container.brz-reset-all { all: unset; }' ) );
+	}
+
+	/**
+	 * Unset all Brizy reset CSS on frontend.
+	 */
+	public function disable_reset_css_on_frontend() {
+		wp_add_inline_style( 'brizy-preview', suki_minify_css_string( '.brz .brz-root__container.brz-reset-all { all: unset; }' ) );
 	}
 
 	/**
