222b39b069fccd509d69fe8d01656831c5ced04e
David Rozando <david.rozando@gmail.com>
2019-05-22 08:15:16 +0700

    Readd Elementor compatibility CSS

--- inc/compatibilities/elementor/class-suki-compatibility-elementor.php
+++ inc/compatibilities/elementor/class-suki-compatibility-elementor.php
@@ -39,6 +39,9 @@ class Suki_Compatibility_Elementor {
 	 * Class constructor
 	 */
 	protected function __construct() {
+		// Compatibility CSS
+		add_filter( 'suki/frontend/inline_css', array( $this, 'add_compatibility_css' ) );
+
 		// Add theme defined fonts to all typography settings.
 		add_action( 'elementor/fonts/additional_fonts', array( $this, 'add_theme_fonts_as_options_on_font_control' ) );
 
@@ -58,6 +61,18 @@ class Suki_Compatibility_Elementor {
 	 * Hook functions
 	 * ====================================================
 	 */
+
+	/**	
+	 * Add compatibility CSS.
+	 *
+	 * @param string $inline_css	
+	 * @return string
+	 */	
+	public function add_compatibility_css( $inline_css ) {
+		$inline_css .= "\n/* Elementor Compatibility CSS */\n" . suki_minify_css_string( '.elementor-text-editor > *:last-child { margin-bottom: 0; }' );
+
+ 		return $inline_css;
+	}
 
 	/**
 	 * Modify Icon control: add fonts.
