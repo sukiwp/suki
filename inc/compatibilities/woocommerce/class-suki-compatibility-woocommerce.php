6dc2d61a22ddc4eea3274cabd380ef4b6b8d512a
David Rozando <david.rozando@gmail.com>
2019-08-13 00:41:11 +0700

    Fix WooCommerce products grid template (for Suki Pro compatibility)

--- inc/compatibilities/woocommerce/class-suki-compatibility-woocommerce.php
+++ inc/compatibilities/woocommerce/class-suki-compatibility-woocommerce.php
@@ -229,7 +229,7 @@ class Suki_Compatibility_WooCommerce {
 		// Reposition product image and wrap it with custom <div>.
 		add_action( 'woocommerce_before_shop_loop_item_title', array( $this, 'render_loop_product_thumbnail_wrapper' ), 1 );
 		add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_link_open', 9 );
-		add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_link_close', 11 );
+		add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_link_close', 19 );
 		add_action( 'woocommerce_before_shop_loop_item_title', array( $this, 'render_loop_product_thumbnail_wrapper_end' ), 999 );
 
 		// Wrap the title with link.
