6703b498a23baa5d88c306db0fb3c1481f1d503e
David Rozando <david.rozando@gmail.com>
2019-08-27 21:31:33 +0700

    Improve widgets' styles

--- inc/compatibilities/woocommerce/customizer/postmessages.php
+++ inc/compatibilities/woocommerce/customizer/postmessages.php
@@ -33,7 +33,7 @@ $add['subtle_color'] = array(
 $add['link_text_color'] = array(
 	array(
 		'type'     => 'css',
-		'element'  => '.woocommerce .woocommerce-error .button, .woocommerce .woocommerce-info .button, .woocommerce .woocommerce-message .button, .woocommerce nav.woocommerce-pagination ul li a:hover, .woocommerce nav.woocommerce-pagination ul li a:focus, .woocommerce div.product .woocommerce-tabs ul.tabs li a:hover, .woocommerce div.product .woocommerce-tabs ul.tabs li a:focus, .woocommerce .woocommerce-breadcrumb a:hover, .woocommerce .woocommerce-breadcrumb a:focus, .woocommerce-account .suki-woocommerce-MyAccount-sidebar a:hover, .woocommerce-account .suki-woocommerce-MyAccount-sidebar a:focus, .woocommerce ul.products li.product a.woocommerce-loop-product__link:hover, .woocommerce ul.products li.product a.woocommerce-loop-product__link:focus',
+		'element'  => '.woocommerce .woocommerce-error .button, .woocommerce .woocommerce-info .button, .woocommerce .woocommerce-message .button, .woocommerce nav.woocommerce-pagination ul li a:hover, .woocommerce nav.woocommerce-pagination ul li a:focus, .woocommerce div.product .woocommerce-tabs ul.tabs li a:hover, .woocommerce div.product .woocommerce-tabs ul.tabs li a:focus, .woocommerce .woocommerce-breadcrumb a:hover, .woocommerce .woocommerce-breadcrumb a:focus, .woocommerce-account .suki-woocommerce-MyAccount-sidebar a:hover, .woocommerce-account .suki-woocommerce-MyAccount-sidebar a:focus, .woocommerce ul.products li.product a.woocommerce-loop-product__link:hover, .woocommerce ul.products li.product a.woocommerce-loop-product__link:focus, .woocommerce .widget_layered_nav_filters li a:hover, .woocommerce .widget_layered_nav_filters li a:focus',
 		'property' => 'color',
 	),
 	array(
@@ -58,7 +58,7 @@ $add['link_text_color'] = array(
 	),
 	array(
 		'type'     => 'css',
-		'element'  => '.woocommerce .woocommerce-widget-layered-nav-list li.chosen a:before, .woocommerce .widget_layered_nav_filters ul li.chosen a:before, .woocommerce .widget_rating_filter ul li.chosen a:before',
+		'element'  => '.woocommerce .widget_layered_nav li.chosen a:before, .woocommerce .widget_rating_filter li.chosen a:before',
 		'property' => 'background-color',
 	),
 );
