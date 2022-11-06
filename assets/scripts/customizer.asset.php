baed3969fe721c1cfa917eda9059efa2733b4cf1
David Rozando <david.rozando@gmail.com>
2022-11-06 15:57:35 +0700

    Tidy up

--- assets/scripts/customizer.asset.php
+++ assets/scripts/customizer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '54a2acfc27626fad85cd');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '036d32514e45899ff0a2');
--- assets/scripts/dashboard.asset.php
+++ assets/scripts/dashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '810e23119ab3290a6379');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'af17a1d7b24d60e0cc41');
