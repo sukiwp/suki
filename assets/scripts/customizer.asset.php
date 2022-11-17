13a4eb6a9c48fbc7a3327cf1e8c4da0378e5b505
David Rozando <david.rozando@gmail.com>
2022-11-17 17:17:32 +0700

    Customizer: Fix toggle control CSS

--- assets/scripts/customizer.asset.php
+++ assets/scripts/customizer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '4d845f718027ee3de146');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5f04a1093a932c66e62f');
