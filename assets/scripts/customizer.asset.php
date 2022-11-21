14145a0481edd0525201bdbac2fd605e3da24df6
David Rozando <david.rozando@gmail.com>
2022-11-21 09:36:34 +0700

    Improve SCSS writing format

--- assets/scripts/customizer.asset.php
+++ assets/scripts/customizer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd9f552ae7a4718da958e');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7a5a09838ad5c59de6df');
