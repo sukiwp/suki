cbe034a908a9c6aaa8852591c0622b852b2a49e5
David Rozando <david.rozando@gmail.com>
2022-12-01 07:03:27 +0700

    Customizer: Specific controls CSS

--- assets/scripts/customizer.asset.php
+++ assets/scripts/customizer.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '2439f3d4e59187c5fbd0');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '920cf3e5d4adf43119e1');
